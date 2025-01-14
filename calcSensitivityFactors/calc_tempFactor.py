d8da643ab653593e4afc8494c15a35e201c3bf33
AneNJ <57398189+AneNJ@users.noreply.github.com>
2025-01-14 18:03:08 +0100

    Update calc_tempFactor.py

--- calcSensitivityFactors/calc_tempFactor.py
+++ calcSensitivityFactors/calc_tempFactor.py
@@ -5,8 +5,8 @@ import numpy as np
 
 # Calculating  for all crops
 
-ISIMIP_dir = "/div/no-backup/users/anenj/ISIMIP/"
-outDir = "tempFactors/"
+ISIMIP_dir = "ISIMIP/" #Path to ISIMIP dataset
+outDir = "../results/tempFactors/"
 
 #################### Create landmask from lpjml file ####################
 f = ISIMIP_dir+"rcp26/co2/lpjml_gfdl-esm2m_ewembi_rcp26_2005soc_co2_yield-ric-firr_global_annual_2006_2099.nc4"
@@ -21,8 +21,8 @@ w = "firr"
 #w = "noirr"
 
 # ********* table and map needed for masking **********
-df_regs = pd.read_csv("../prepareMaskStuff/countryCodesWithMapNr.csv",index_col="code2")
-ds_regs = xr.open_dataset("../prepareMaskStuff/GPW3_countries_0_5deg_2011_27315.nc")
+df_regs = pd.read_csv("countryCodesWithMapNr.csv",index_col="code2")
+ds_regs = xr.open_dataset("GPW3_countries_0_5deg_2011_27315.nc") #Country def file
 # *****************************************************
 
 #cropMods = ["CLM45", "GEPIC", "LPJmL", "PEPIC"]
