17d3d8e6b2af25f12ab8fbe39c7d37e7c194aea5
AneNJ <57398189+AneNJ@users.noreply.github.com>
2025-06-27 17:44:46 +0200

    Add files via upload

--- plotScripts/plotFig5.py
+++ plotScripts/plotFig5.py
@@ -5,7 +5,7 @@ import xarray as xr
 import numpy as np
 import os
 
-tempFactor_dir = "/div/qbo/users/anenj/GTDR/calcSensitivityFactors/tempFactors/"
+tempFactor_dir = "/div/qbo/users/anenj/GTDR/calcSensitivityFactors/concFactors/"
 
 regions = ['AT', 'AU', 'BE', 'BG', 'BR', 'CA', 'CH',
            'CN', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES',
@@ -15,11 +15,9 @@ regions = ['AT', 'AU', 'BE', 'BG', 'BR', 'CA', 'CH',
            'RO', 'RU', 'SE', 'SI', 'SK', 'TR', 'US',
            'ZA']
 
-#(SKIPPING TW FROM LIST OF REGIONS SINCE IT'S NOT IN THE HYBRID VERSION)
-
-yearint = "15"   #number of years to average over
-st = "5"         #skipping first 5 years 
+st = "5"  #first timestep included
 w = "firr"
+
 fig,axs = plt.subplots(4,1,figsize=(10,10),dpi=600)
 
 forcingMods = ["GFDL-ESM2M", "HadGEM2-ES",
@@ -42,18 +40,19 @@ cropList = ["soy","mai","whe","ric"]
 
 label = False
 for r,reg in enumerate(regions):
-    f = reg+"_"+w+"_"+st+"_"+yearint+".csv"
+    f = reg+"_"+w+"_"+st+".csv"
     df = pd.read_csv(tempFactor_dir+f,index_col=[0,1],header=[0,1])
+    #print(df)
     for c,crop in enumerate(cropList):
         plt.sca(axs[c])
         for case in cases:
             values = df.loc[(case.split("_")[0],crop),(case.split("_")[1],forcingMods)].astype(float)
             if not label:
-                plt.scatter(r,np.mean(values), s=sizeDict[case], color=colDict[case],label=labelDict[case])
+                plt.scatter(r,np.mean(values), s=sizeDict[case], color=colDict[case], label=labelDict[case])
                 if case==cases[-1]:
                     label = True
             else:
-                plt.scatter(r,np.mean(values), s=sizeDict[case],color=colDict[case])
+                plt.scatter(r,np.mean(values), s=sizeDict[case], color=colDict[case])
                 if np.isnan(np.mean(values)):
                     plt.scatter(r,0,marker="x",color=colDict[case])
             plt.title(crop,x=0.95,y=0,fontsize=30)
@@ -68,14 +67,14 @@ axs[0].axhline(y=0, color ="black", linestyle='--',linewidth=0.8)
 axs[1].axhline(y=0, color ="black", linestyle='--',linewidth=0.8)
 axs[2].axhline(y=0, color ="black", linestyle='--',linewidth=0.8)
 axs[3].axhline(y=0, color ="black", linestyle='--',linewidth=0.8)
-axs[0].set_ylabel("[1/K]",font={"size":15})
-axs[1].set_ylabel("[1/K]",font={"size":15})
-axs[2].set_ylabel("[1/K]",font={"size":15})
-axs[3].set_ylabel("[1/K]",font={"size":15})
-axs[0].set_ylim([-1,1])
-axs[1].set_ylim([-1,1])
-axs[2].set_ylim([-1,1])
-axs[3].set_ylim([-1,1])
+axs[0].set_ylabel("[1/ppm]",font={"size":15})
+axs[1].set_ylabel("[1/ppm]",font={"size":15})
+axs[2].set_ylabel("[1/ppm]",font={"size":15})
+axs[3].set_ylabel("[1/ppm]",font={"size":15})
+axs[0].set_ylim([-0.001,0.003])
+axs[1].set_ylim([-0.001,0.003])
+axs[2].set_ylim([-0.001,0.003])
+axs[3].set_ylim([-0.001,0.003])
 axs[0].set_xlim([-0.5, 42.5])
 axs[1].set_xlim([-0.5, 42.5])
 axs[2].set_xlim([-0.5, 42.5])
@@ -83,12 +82,13 @@ axs[3].set_xlim([-0.5, 42.5])
 
 letters = ["A)","B)","C)","D)"]
 for n,ax in enumerate(axs.flat):  
-    ax.text(-0.11, 0.92, letters[n], transform=ax.transAxes, 
+    ax.text(-0.135, 0.92, letters[n], transform=ax.transAxes, 
             size=20, weight='bold')
 
 fig.legend(bbox_to_anchor=(0.176,0.93),loc="upper left",fontsize=15,ncol=3)
 
-fName = "plots/figure4.png"
+fName = "plots/figure5.png"
 plt.savefig(fName)
 #cmd = "display "+fName+" &"
 #os.system(cmd)
+
